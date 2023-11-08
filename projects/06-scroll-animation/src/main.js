52d361b46fd53ddaf3bc3de7848b74d12b14c64d
francisco-solis99 <franciscosoliscss@gmail.com>
2023-11-08 17:56:24 -0600

    feat(06-scroll-animation): :sparkles: add animation when scroll to the cards using intersection observer

---
+++ projects/06-scroll-animation/src/main.js
@@ -0,0 +1,20 @@
+// Doing with the intersection observer
+const cards = document.querySelectorAll('.card');
+
+const observerOptions = {
+  root: null,
+  rootMargin: '0px',
+  threshold: '0.5'
+};
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    entry.target.classList.toggle('show', entry.isIntersecting);
+    if (entry.isIntersecting) {
+      // Unobserve just once
+      observer.unobserve(entry.target);
+    }
+  });
+}, observerOptions);
+
+cards.forEach(card => observer.observe(card));
