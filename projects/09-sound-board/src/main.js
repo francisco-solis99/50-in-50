6e810f25168a18de723f5ca02b786b685e6da04f
francisco-solis99 <franciscosoliscss@gmail.com>
2023-11-17 00:10:43 -0600

    feat(09-sound-cards): :sparkles: add the sound when click the sound card

--- projects/09-sound-board/src/main.js
+++ projects/09-sound-board/src/main.js
@@ -1,14 +1,23 @@
 import sounds from './sounds.json' assert { type:'json' }
-
 const soundsCardsWrapper = document.querySelector('.sounds-cards__wrapper');
 
+
 function createSoundCard (sound) {
+  const {name: soundName, file: soundFile} = sound
   const card = document.createElement('article');
   card.classList.add('sound-card')
-  card.textContent = sound.name
+  card.textContent = soundName
+
+  card.addEventListener('click', () => {
+    const audio = new Audio()
+    audio.preload = 'auto';
+    audio.src = `../public/sounds/${soundFile}`
+    audio.currentTime = 0;
+    audio.play();
+  })
+
   return card
 }
 
 const soundCards = sounds.map(createSoundCard)
-
 soundsCardsWrapper.append(...soundCards)
