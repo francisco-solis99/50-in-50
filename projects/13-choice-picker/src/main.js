e09b30c7a4900763d8541a665ae41dfd6a16f916
francisco-solis99 <franciscosoliscss@gmail.com>
2023-11-28 13:35:41 -0600

    feat(13-choices-picker): :sparkles: Add only the new tags to the DOM, keep the one who were previously

--- projects/13-choice-picker/src/main.js
+++ projects/13-choice-picker/src/main.js
@@ -1,6 +1,6 @@
 /*
   Improve performance
-  - Add only the new tags to the DOM, keep the one who were previously
+  - Add only the new tags to the DOM, keep the one who were previously ✅
   - Change the tags text from the ones really changed ✅
   - Delete only the ones removed from the list of tags
   - Evalute if there is not text in the textArea to does not do anything
@@ -15,8 +15,10 @@ const textArea = document.querySelector('textarea');
 const tagsList = document.createElement('ul');
 
 textArea.addEventListener('input', (e) => {
-  const currentTags = document.querySelectorAll('.tag');
+  const currentTags = document.querySelectorAll('.tag') ?? [];
   const tagsArr = textArea.value.split(',').filter(item => item.trim());
+
+  // UI CHANGES
 
   // if there is the same tags in the DOM than the tagsArr just change the changes on the text
   if (tagsArr.length === currentTags.length) {
@@ -27,16 +29,18 @@ textArea.addEventListener('input', (e) => {
     return;
   }
 
-  const tags = tagsArr.map((tagText, index) => createTag(tagText));
-  tagsList.replaceChildren(...tags);
+  // Add new tags
+  if (tagsArr.length > currentTags.length) {
+    const start = currentTags.length ? tagsArr.length - 1 : 0;
+    tagsArr.slice(start, tagsArr.length).forEach(tag => {
+      const newTag = createTag(tag);
+      tagsList.append(newTag);
+    });
 
-  // Add the list to the DOM only the first time
-  if (!tagsList.isConnected) container.append(tagsList);
+    // Add the list to the DOM only the first time
+    if (!tagsList.isConnected) container.append(tagsList);
+  }
 });
-
-// function changeTagsList() {
-
-// }
 
 function createTag (tag) {
   const tagElement = document.createElement('li');
